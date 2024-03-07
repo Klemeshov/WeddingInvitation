cc82b52db7506c0d6300d7b2313c029601c488d1
Klemeshov <dima-0510@mail.ru>
2024-03-07 21:54:32 +0300

    add guests info

--- src/guestsInfo.ts
+++ src/guestsInfo.ts
@@ -1,14 +1,66 @@
 export const guestsInfo: Record<string, { name: string; where: 'registry' | 'hall' }> = {
-  'fac86fb6-b5cb-4027-804e-b6ad537b35a4': {
-    name: 'Алексей Иванович',
+  Xm9: {
+    name: 'Дорогие Мама и Папа, любимый Брат',
     where: 'registry'
   },
-  '60d9f23a-bab3-4454-b37f-80204ff6779a': {
-    name: 'Наталья Семеновна',
+  KW0: {
+    name: 'Дорогой Егор',
     where: 'registry'
   },
-  'bf93931f-035f-4e24-9230-347581d56747': {
-    name: 'Мама и Папа',
+  axK: {
+    name: 'Дорогая тетя Лена',
     where: 'registry'
+  },
+  Bx2: {
+    name: 'Любимые Оля и Алиса',
+    where: 'registry'
+  },
+  fDq: {
+    name: 'Дорогой дядя Андрей',
+    where: 'registry'
+  },
+  spc: {
+    name: 'Мояшкины Анна и Данил',
+    where: 'hall'
+  },
+  mjf: {
+    name: 'Роднулькины Полина и Александр',
+    where: 'hall'
+  },
+  zpq: {
+    name: 'Любимкины Ирина и Иван',
+    where: 'hall'
+  },
+  rhn: {
+    name: 'Анжела и Владислав',
+    where: 'hall'
+  },
+  aio: {
+    name: 'Юлия и Дмитрий',
+    where: 'hall'
+  },
+  g4p: {
+    name: 'Дорогой Сашулькин',
+    where: 'hall'
+  },
+  w2m: {
+    name: 'Дорогой Ваня',
+    where: 'hall'
+  },
+  j61: {
+    name: 'Дорогой Миша',
+    where: 'hall'
+  },
+  jk0: {
+    name: 'Виктория и Владимир',
+    where: 'hall'
+  },
+  cx1: {
+    name: 'Дорогой Лёва',
+    where: 'hall'
+  },
+  p9b: {
+    name: 'Мария и Игорь',
+    where: 'hall'
   }
 };
